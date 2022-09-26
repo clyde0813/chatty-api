d9461051e7b3ec1c1a24a2ce5ed240f58c525ac2
clyde0813 <eric3285@naver.com>
2022-09-26 16:33:45 +0900

    STATIC ROOT PATH ADDED

--- chatty_drf/settings.py
+++ chatty_drf/settings.py
@@ -17,6 +17,8 @@ from django.core.exceptions import ImproperlyConfigured
 import pymysql
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
 secret_file = os.path.join(BASE_DIR, 'Secrets.json')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -149,6 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
