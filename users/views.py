d0e1e6da5747ec5236ffd285392e5a39ebe865cf
clyde0813 <eric3285@naver.com>
2022-09-28 14:07:05 +0900

    login - return username, token (requested by frontend dv)

--- users/views.py
+++ users/views.py
@@ -16,7 +16,6 @@ from .serializers import RegisterSerializer, LoginSerializer, LogoutSerializer, 
 from .permissions import CustomReadOnly
 
 
-
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
@@ -30,7 +29,7 @@ class LoginView(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         token = serializer.validated_data
-        return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'username': token.user.username, 'token': token.key}, status=status.HTTP_200_OK)
 
 
 class LogoutView(generics.GenericAPIView):
