d3ddf1b624c48b8613e2d0ac75d2f124c2b2a81b
clyde0813 <eric3285@naver.com>
2022-10-05 23:57:03 +0900

    chat nickname added

--- chats/serializers.py
+++ chats/serializers.py
@@ -16,7 +16,14 @@ class ChatRoomEnteranceSerializer(serializers.ModelSerializer):
 
 class ChatSerializer(serializers.ModelSerializer):
     sender_profile = ProfileSerializer(read_only=True)
+    sender_nickname = serializers.SerializerMethodField('get_sender_nickname', required=False, read_only=True)
 
     class Meta:
         model = Chat
-        fields = ('chatroom', 'sender_profile', 'sender_ip', 'content', 'created_date')
+        fields = ('chatroom', 'sender_profile', 'sender_ip', 'sender_nickname', 'content', 'created_date')
+
+    def get_sender_nickname(self, obj):
+        if obj.sender_profile is None:
+            return obj.chatroom.question.nickname
+        else:
+            return None
