9020172279b9ec29f2a0199460788161d53a30a5
clyde0813 <eric3285@naver.com>
2022-11-16 14:44:46 +0900

    users app testcase added

--- users/tests/test_users.py
+++ users/tests/test_users.py
@@ -10,7 +10,6 @@ class UserTestCase(APITestCase):
         self.register_url = "/api/v1/users/register"
         self.profile_url = "/api/v1/users/profile/"
         self.profile_put_url = "/api/v1/users/profile"
-        # self.email_verify_url = "/api/v1/users/email/verify"
         self.follow_url = "/api/v1/users/follow"
         self.login_url = "/api/v1/users/login"
         self.logout_url = "/api/v1/users/logout"
@@ -22,41 +21,78 @@ class UserTestCase(APITestCase):
         self.username2 = "test2"
         self.email2 = "test2@chatty.kr"
 
-    def test(self):
+    def register_user(self, username, password, email):
         # First User Register
         # self.client.post(self.email_verify_url, {"email": self.email, "username": self.username})
         self.test_case = {
+            "username": username,
+            "password": password,
+            "password2": password,
+            "email": email,
+        }
+        response = self.client.post(self.register_url, self.test_case)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def login(self, username, password):
+        # User 1 Login
+        response = self.client.post(self.login_url, {"username": username, "password": password})
+        self.client = APIClient(HTTP_AUTHORIZATION='Token ' + response.data["token"])
+
+    def test_register(self):
+        self.register_user(self.username, self.password, self.email)
+
+    def test_forbidden_username_register(self):
+        self.test_case = {
+            "username": "admin",
+            "password": self.password,
+            "password2": self.password,
+            "email": self.email2,
+        }
+        response = self.client.post(self.register_url, self.test_case)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_duplicate_username_register(self):
+        self.register_user(self.username, self.password, self.email)
+        self.test_case = {
             "username": self.username,
+            "password": self.password,
+            "password2": self.password,
+            "email": self.email2,
+        }
+        response = self.client.post(self.register_url, self.test_case)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_duplicate_email_register(self):
+        self.register_user(self.username, self.password, self.email)
+        self.test_case = {
+            "username": self.username2,
             "password": self.password,
             "password2": self.password,
             "email": self.email,
         }
         response = self.client.post(self.register_url, self.test_case)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_login(self):
+        self.register_user(self.username, self.password, self.email)
+        self.login(self.username, self.password)
+
+    def test_profile(self):
+        self.register_user(self.username, self.password, self.email)
+        self.register_user(self.username2, self.password, self.email2)
         # Check User 1 Profile
         response = self.client.get(self.profile_url + self.username)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # Second User Register
-        # self.client.post(self.email_verify_url, {"email": self.email2, "username": self.username2})
-        self.test_case = {
-            "username": self.username2,
-            "password": self.password,
-            "password2": self.password,
-            "email": self.email2,
-            # "verification_code": str(cache.get(self.email2))
-        }
-        response = self.client.post(self.register_url, self.test_case)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # Check User 2 Profile
         response = self.client.get(self.profile_url + self.username2)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # User 1 Login
-        response = self.client.post(self.login_url, {"username": self.username, "password": self.password})
-        self.client = APIClient(HTTP_AUTHORIZATION='Token ' + response.data["token"])
+    def test_follow(self):
+        self.register_user(self.username, self.password, self.email)
+        self.register_user(self.username2, self.password, self.email2)
+
+        self.login(self.username, self.password)
 
         # User 1 Follow User 2
         response = self.client.post(self.follow_url, {"username": self.username2})
@@ -66,6 +102,8 @@ class UserTestCase(APITestCase):
         response = self.client.get(self.profile_url + self.username2)
         self.assertEqual(len(response.data["follower"]), 1)
 
+    def test_unfollow(self):
+        self.test_follow()
         # User 1 Unfollow User 2
         response = self.client.post(self.follow_url, {"username": self.username2})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -74,14 +112,16 @@ class UserTestCase(APITestCase):
         response = self.client.get(self.profile_url + self.username2)
         self.assertEqual(len(response.data["follower"]), 0)
 
+    def test_logout(self):
+        self.register_user(self.username, self.password, self.email)
+        self.login(self.username, self.password)
         # User 1 Logout
         response = self.client.get(self.logout_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.client = APIClient()
 
-        # User 1 Login
-        response = self.client.post(self.login_url, {"username": self.username, "password": self.password})
-        self.client = APIClient(HTTP_AUTHORIZATION='Token ' + response.data["token"])
+    def test_profile_message_update(self):
+        self.register_user(self.username, self.password, self.email)
+        self.login(self.username, self.password)
 
         # User 1 Change profile_message
         response = self.client.put(self.profile_put_url, {"profile_message": "test"})
@@ -90,3 +130,27 @@ class UserTestCase(APITestCase):
         # Check User 1 profile_message
         response = self.client.get(self.profile_url + self.username)
         self.assertEqual(response.data["profile_message"], "test")
+
+    def test_profile_username_update(self):
+        self.register_user(self.username, self.password, self.email)
+        self.login(self.username, self.password)
+
+        # User 1 Change profile_message
+        response = self.client.put(self.profile_put_url, {"username": "test1234"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check User 1 profile_message
+        response = self.client.get(self.profile_url + "test1234")
+        self.assertEqual(response.data["username"], "test1234")
+
+    def test_profile_forbidden_username_update(self):
+        self.register_user(self.username, self.password, self.email)
+        self.login(self.username, self.password)
+
+        # User 1 Change profile_message
+        response = self.client.put(self.profile_put_url, {"username": "admin"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Check User 1 profile_message
+        response = self.client.get(self.profile_url + self.username)
+        self.assertEqual(response.data["username"], self.username)
