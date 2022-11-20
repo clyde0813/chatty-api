37e1b13873b6b1b2298e528f5d383a73433b7097
clyde0813 <eric3285@naver.com>
2022-11-20 13:59:40 +0900

    DEV Database deleted

--- config/settings/base.py
+++ config/settings/base.py
@@ -142,6 +142,25 @@ REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_secret('NAME'),
+        'TEST': {
+            'NAME': get_secret('TEST_NAME'),
+        },
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': get_secret('HOST'),
+        'PORT': get_secret('PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        }
+    },
+}
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
