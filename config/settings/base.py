df0015111fda89f8a01ff834e22f96a276bbce80
clyde0813 <eric3285@naver.com>
2023-04-11 15:18:23 +0900

    JWT - UPDATE_LAST_LOGIN, ROTATE_REFRESH_TOKENS False

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,9 +45,9 @@ SECRET_KEY = get_secret("SECRET_KEY")
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": True,
+    "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
-    "UPDATE_LAST_LOGIN": True,
+    "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
