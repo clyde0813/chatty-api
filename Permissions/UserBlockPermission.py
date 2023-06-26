c449fac86a9da48986501dba7fbd6ad2fa87ee64
clyde0813 <eric3285@naver.com>
2023-06-26 13:25:41 +0900

    Permissions : IsBlockedOneWay bug fixed

--- Permissions/UserBlockPermission.py
+++ Permissions/UserBlockPermission.py
@@ -8,22 +8,21 @@ from user.models import BlockedProfile
 
 class IsBlockedOneWay(permissions.BasePermission):
     def has_permission(self, request, view):
-        def has_permission(self, request, view):
-            if request.user.is_authenticated is not True:
-                return True
-
-            # 차단한 유저의 정보 요청 필터링
-            # 2023.06.25
-            if request.method == 'GET':
-                if BlockedProfile.objects.filter(profile__user__username=view.kwargs['username'],
-                                                 blocked_profile=request.user.profile).exists():
-                    raise DataInaccuracyError()
-            else:
-                if BlockedProfile.objects.filter(profile__user__username=request.data['username'],
-                                                 blocked_profile=request.user.profile).exists():
-                    raise DataInaccuracyError()
-
+        if request.user.is_authenticated is not True:
             return True
+
+        # 차단한 유저의 정보 요청 필터링
+        # 2023.06.25
+        if request.method == 'GET':
+            if BlockedProfile.objects.filter(profile__user__username=view.kwargs['username'],
+                                             blocked_profile=request.user.profile).exists():
+                raise DataInaccuracyError()
+        else:
+            if BlockedProfile.objects.filter(profile__user__username=request.data['username'],
+                                             blocked_profile=request.user.profile).exists():
+                raise DataInaccuracyError()
+
+        return True
 
 
 class IsBlockedTwoWay(permissions.BasePermission):
