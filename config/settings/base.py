b4226e86ee84cda1afd2d94e3508d6af048eae36
clyde0813 <eric3285@naver.com>
2023-07-05 05:07:57 +0900

    static_root fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -199,7 +199,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(ROOT_DIR, '../../../../.static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 #
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
